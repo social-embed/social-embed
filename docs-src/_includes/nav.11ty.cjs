a8161a105309abb98e466664af4f0044be48e054
Tony Narlock <tony@git-pull.com>
2021-03-27 10:31:23 -0500

    Docs: Top navbar updates

--- docs-src/_includes/nav.11ty.cjs
+++ docs-src/_includes/nav.11ty.cjs
@@ -2,14 +2,11 @@ const relative = require('./relative-path.cjs');
 
 module.exports = function ({page}) {
   return `
-<nav>
+<nav id="main-menu">
   <a href="${relative(page.url, '/')}">Home</a>
   <a href="${relative(page.url, '/examples/')}">Examples</a>
   <a href="${relative(page.url, '/api/')}">API</a>
   <a href="${relative(page.url, '/install/')}">Install</a>
   <a href="${relative(page.url, '/changes/')}">Release notes</a>
-  <a href="https://www.npmjs.com/package/@tony/oembed-component" target="_blank" rel="noopener noreferrer">NPM</a>
-  <a href="https://github.com/tony/oembed-component" target="_blank" rel="noopener noreferrer">GitHub</a>
-  <a href="https://codepen.io/attachment/pen/bGBZGEv" target="_blank" rel="noopener noreferrer">CodeSandbox Demo</a>
 </nav>`;
 };
