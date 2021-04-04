ceef69f49f5ca013be89e3eef2617cab16f59336
Tony Narlock <tony@git-pull.com>
2021-04-04 14:12:43 -0500

    wc: streamline tests with esbuild

--- packages/wc/web-test-runner.config.js
+++ packages/wc/web-test-runner.config.js
@@ -1,20 +1,17 @@
 process.env.NODE_ENV = 'test';
 import {defaultReporter} from '@web/test-runner';
-import {default as specReporter} from './spec-reporter.js';
+import {esbuildPlugin} from '@web/dev-server-esbuild';
 import {importMapsPlugin} from '@web/dev-server-import-maps';
+import {default as specReporter} from './spec-reporter.js';
 
 export default {
   plugins: [
+    esbuildPlugin({ts: true}),
     importMapsPlugin({
       inject: {
         importMap: {
           imports: {
-            // mock a dependeny
-            '../dist/index.js': './dist/index.js',
-            '@social-embed/wc': './dist/index.js',
-            'package-a': '/mocks/package-a.js',
-            // mock a module in your own code
-            '/src/my-module.js': '/mocks/my-module.js',
+            '@social-embed/wc': './index.ts',
           },
         },
       },
