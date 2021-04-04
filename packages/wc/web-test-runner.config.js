e2302a7b62fbb3868999b936241ae4fd9667d786
Tony Narlock <tony@git-pull.com>
2021-04-04 14:33:57 -0500

    wc: Silence custom runner for now

--- packages/wc/web-test-runner.config.js
+++ packages/wc/web-test-runner.config.js
@@ -2,7 +2,7 @@ process.env.NODE_ENV = 'test';
 import {defaultReporter} from '@web/test-runner';
 import {esbuildPlugin} from '@web/dev-server-esbuild';
 import {importMapsPlugin} from '@web/dev-server-import-maps';
-import {default as specReporter} from './spec-reporter.js';
+// import {default as specReporter} from './spec-reporter.js';
 
 export default {
   plugins: [
@@ -19,6 +19,6 @@ export default {
   ],
   reporters: [
     defaultReporter({reportTestResults: true, reportTestProgress: true}),
-    specReporter(),
+    // specReporter(),
   ],
 };
