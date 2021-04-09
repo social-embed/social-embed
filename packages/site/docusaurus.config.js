bfd7a11076236e0c0d755bde679baa2d1180003a
Tony Narlock <tony@git-pull.com>
2021-04-08 20:40:15 -0500

    docs: Fix google tracking link

--- packages/site/docusaurus.config.js
+++ packages/site/docusaurus.config.js
@@ -16,7 +16,6 @@ module.exports = {
   plugins: [
     path.resolve(__dirname, 'plugins', 'more-mdx-paths'),
     '@docusaurus/theme-live-codeblock',
-    '@docusaurus/plugin-google-gtag',
     [
       'docusaurus-plugin-typedoc',
 
@@ -210,7 +209,9 @@ module.exports = {
       indexName: 'social-embed',
       contextualSearch: true,
     },
-    gtag: {trackingID: 'G-6LZEGR7SKM'},
+    googleAnalytics: {
+      trackingID: 'G-6LZEGR7SKM',
+    },
   },
   presets: [
     [
