290c368624c1333a473a9c90ad7e22e44889fe0a
Tony Narlock <tony@git-pull.com>
2023-11-11 09:12:27 -0600

    docs(sidebar): Remove extra, unlinked breadcrumb level
    This was adding "@social-embed" item breadcrumb after home, which
    was not linked.

--- packages/site/sidebars.ts
+++ packages/site/sidebars.ts
@@ -2,53 +2,49 @@ import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
 
 const sidebars: SidebarsConfig = {
   sidebar: [
+    "home",
     {
-      "@social-embed": [
-        "home",
+      type: "category",
+      label: "⚙️ @social-embed/lib",
+      collapsed: false,
+      items: ["lib/index", "lib/installation", "lib/release-notes"],
+    },
+    {
+      type: "category",
+      label: "▶️ @social-embed/wc",
+      collapsed: false,
+      items: [
+        "wc/index",
+        "wc/installation",
         {
           type: "category",
-          label: "⚙️ @social-embed/lib",
-          collapsed: false,
-          items: ["lib/index", "lib/installation", "lib/release-notes"],
-        },
-        {
-          type: "category",
-          label: "▶️ @social-embed/wc",
+          label: "Providers",
           collapsed: false,
           items: [
-            "wc/index",
-            "wc/installation",
-            {
-              type: "category",
-              label: "Providers",
-              collapsed: false,
-              items: [
-                "wc/providers/generic",
-                "wc/providers/youtube",
-                "wc/providers/dailymotion",
-                "wc/providers/spotify",
-                "wc/providers/vimeo",
-                "wc/providers/wistia",
-                "wc/providers/loom",
-                "wc/providers/edpuzzle",
-              ],
-            },
-            {
-              type: "category",
-              label: "Configuration",
-              collapsed: false,
-              items: [
-                "wc/configuration/dimensions",
-                "wc/configuration/allowfullscreen",
-                "wc/configuration/url",
-              ],
-            },
-            "wc/release-notes",
+            "wc/providers/generic",
+            "wc/providers/youtube",
+            "wc/providers/dailymotion",
+            "wc/providers/spotify",
+            "wc/providers/vimeo",
+            "wc/providers/wistia",
+            "wc/providers/loom",
+            "wc/providers/edpuzzle",
           ],
         },
-        "news",
+        {
+          type: "category",
+          label: "Configuration",
+          collapsed: false,
+          items: [
+            "wc/configuration/dimensions",
+            "wc/configuration/allowfullscreen",
+            "wc/configuration/url",
+          ],
+        },
+        "wc/release-notes",
       ],
     },
+    "news",
   ],
 };
 
