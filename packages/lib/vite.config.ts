f41435a21d94101129a71a5e49322c2149477bf2
Tony Narlock <tony@git-pull.com>
2024-06-08 06:59:25 -0500

    lib(vite): Fetch via `path.resolve()`

--- packages/lib/vite.config.ts
+++ packages/lib/vite.config.ts
@@ -1,10 +1,11 @@
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: "src/index.ts",
+      entry: resolve(__dirname, "src/index.ts"),
       name: "SocialEmbedLib",
     },
   },
