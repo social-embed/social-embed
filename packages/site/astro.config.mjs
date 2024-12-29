b724a65a42a84eee1f9f8a53867a03f5a28bb538
Tony Narlock <tony@git-pull.com>
2024-12-29 10:48:58 -0600

    docs: Add live code to sidebar

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -17,6 +17,7 @@ export default defineConfig({
           items: [
             // Each item here is one entry in the navigation menu.
             { label: "Example Guide", slug: "guides/example" },
+            { label: "Live Code", slug: "guides/live-code" },
           ],
         },
         {
