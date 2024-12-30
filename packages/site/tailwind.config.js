7b906d4aa076cc2bd7208b35b453787bc277dc34
Tony Narlock <tony@git-pull.com>
2024-12-30 15:13:38 -0600

    docs(tailwind) Use IBM Plex fonts

--- packages/site/tailwind.config.js
+++ packages/site/tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 import starlightPlugin from "@astrojs/starlight-tailwind";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"IBM Plex Sans"', ...defaultTheme.fontFamily.sans],
+        mono: ['"IBM Plex Mono"', ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
   plugins: [starlightPlugin()],
 };
