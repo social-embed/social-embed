6b75b0f20c2a79c1537276aba5fbd9bd33212a34
Tony Narlock <tony@git-pull.com>
2024-12-30 15:00:09 -0600

    astro(config) Add `site`
    See also: https://starlight.astro.build/guides/customization/#enable-sitemap

--- packages/site/astro.config.mjs
+++ packages/site/astro.config.mjs
@@ -7,6 +7,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://social-embed.git-pull.com",
   integrations: [
     starlight({
       title: "social-embed",
