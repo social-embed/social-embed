b50c850b078372eff566379afa65e77e57838463
Tony Narlock <tony@git-pull.com>
2025-01-04 18:41:30 -0600

    lib: Add DailyMotionProvider

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -7,9 +7,12 @@ export {
   getProviderFromUrl,
   convertUrlToEmbedUrl,
 } from "./utils";
+import { DailyMotionProvider } from "./providers/dailymotion";
 import { YouTubeProvider } from "./providers/youtube";
 import { EmbedProviderRegistry } from "./registry";
 
 // Create an instance of the registry
 export const defaultRegistry = new EmbedProviderRegistry();
+defaultRegistry.register(DailyMotionProvider);
 defaultRegistry.register(YouTubeProvider);
+
