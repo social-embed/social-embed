15fd8f5df4d657285760edde1f6329e0e77ad10a
Tony Narlock <tony@git-pull.com>
2025-01-05 04:05:20 -0600

    refactor(lib[providers]) Remove more `Provider` reliance

--- packages/lib/src/types.ts
+++
@@ -1,5 +0,0 @@
-import type { Provider } from "./constants";
-
-export type ProviderKey = keyof typeof Provider;
-export type ProviderType = (typeof Provider)[Provider];
-export type ValueOfProvider = `${Provider}`;
